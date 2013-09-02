ca1bb871623843b981bec1966147a888829e603e
Sharron Denice <sharron@me.com>
2013-09-02 03:04:41 -0500

    Ticket #TH-1

--- tools.remove_hidden_files.php
+++ tools.remove_hidden_files.php
@@ -1,13 +1,11 @@
 <?php 
 
 	echo "Removing hidden files. . .";
-
 	system("find . -name '.svn' -exec rm -rf {} \;");
 	system("find . -name '_notes' -exec rm -rf {} \;");
 	system("find . -name '.DS_Store' -exec rm -rf {} \;");
 	system("find . -name '._*' -exec rm -rf {} \;");
 	system("find . -name '__MACOSX' -exec rm -rf {} \;");
-
 	echo "done!";
 	
 ?>
