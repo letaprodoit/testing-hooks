9eaf5379fb99c6e55f9a9846b1b5dafba56a9619
Sharron Denice <sharron@me.com>
2013-09-02 03:10:38 -0500

    Ticket #TH-1

--- tools.remove_hidden_files.php
+++ tools.remove_hidden_files.php
@@ -1,11 +1,9 @@
 <?php 
-
 	echo "Removing hidden files. . .";
 	system("find . -name '.svn' -exec rm -rf {} \;");
 	system("find . -name '_notes' -exec rm -rf {} \;");
 	system("find . -name '.DS_Store' -exec rm -rf {} \;");
 	system("find . -name '._*' -exec rm -rf {} \;");
 	system("find . -name '__MACOSX' -exec rm -rf {} \;");
-	echo "done!";
-	
+	echo "done!";	
 ?>
